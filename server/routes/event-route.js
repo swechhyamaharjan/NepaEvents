d22909137cd5f5e99e6d4d0161f6d9b06611680a
Swechhya maharjan <swekchya.mjn@gmail.com>
2025-02-24 17:48:58 +0545

    first commit

--- server/routes/event-route.js
+++ server/routes/event-route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {createEvent, getAllEvents} = require('../controller/event-controller')
+const {createEvent, getAllEvents, updateEvent,} = require('../controller/event-controller')
 
 const eventRouter = express.Router();
 
 eventRouter.post('/event', createEvent)
 eventRouter.get('/events', getAllEvents)
+eventRouter.patch('/:eventId', updateEvent)
+// eventRouter.delete('/events/:eventsId', deleteEvent)
 
 module.exports = eventRouter;
--- server/routes/venue-route.js
+++ server/routes/venue-route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {bookVenue, getAllVenues} = require('../controller/venue-controller')
+const {bookVenue, getAllVenues, updateVenue} = require('../controller/venue-controller')
 
 const venueRouter = express.Router();
 
 venueRouter.post('/venue', bookVenue)
 venueRouter.get('/venues', getAllVenues)
+venueRouter.patch('/:venueId', updateVenue)
 
 module.exports = venueRouter;
