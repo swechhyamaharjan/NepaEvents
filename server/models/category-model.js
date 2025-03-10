782a263db415604809498d699e5ad8c4d5584b71
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-03-10 14:43:19 +0545

    Add image field and enable timestamps in category model

--- server/models/category-model.js
+++ server/models/category-model.js
@@ -1,15 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, 
   }
-}, {
-  timeStampes: true
-})
+);
 
 const Category = mongoose.model("Category", categorySchema);
 
