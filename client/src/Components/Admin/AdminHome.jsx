4f0252344a35d9d41711a734f77fab1a4e7c5f53
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-03-19 23:05:14 +0545

    Display full name of the user in AdminHome component

--- client/src/Components/Admin/AdminHome.jsx
+++ client/src/Components/Admin/AdminHome.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../Context/AuthContext';
 
 export const AdminHome = () => {
   const navigate = useNavigate();
-
+const {user} = useAuth();
   return (
     <div className="min-h-screen bg-gray-100 p-8 font-sans">
+      {console.log(user)}
       {/* Subtle Background Pattern */}
       <div className="absolute inset-0 bg-grid-black/[0.02] bg-[size:30px_30px] pointer-events-none" />
       
       {/* Admin Header */}
       <div className='w-full flex items-center justify-end pb-10'>
         <div className='mr-8 bg-white px-6 py-3 rounded-lg shadow-md'>
-          <h2 className="text-gray-700 font-medium">Welcome, <span className="text-gray-900 font-bold"></span></h2>
+          <h2 className="text-gray-700 font-medium">Welcome, <span className="text-gray-900 font-bold">{user.user.fullName}</span></h2>
         </div>
         <button 
           className='logoutbtn bg-[#ED4A43] text-white py-3 px-8 rounded-lg shadow-md hover:shadow-lg hover:bg-opacity-90 transition-all duration-300' 
