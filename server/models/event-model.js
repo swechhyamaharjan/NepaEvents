c69cd519e73e785576b3ba97ace8778d968e284b
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-03-21 22:55:40 +0545

    Enhanced event and venue models

--- server/models/event-model.js
+++ server/models/event-model.js
@@ -4,11 +4,12 @@ const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: Date, required: true },
+  category: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
   venue: { type: mongoose.Schema.Types.ObjectId, ref: "Venue", required: true },
   // artist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Artist", required: true }],
   price: { type: Number, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+  ticket_sold: { type: Number, default: 0 },
+  organizer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now },
   image: { type: String, required: true }
 });
--- server/models/venue-model.js
+++ server/models/venue-model.js
@@ -4,7 +4,9 @@ const venueSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
   capacity: { type: Number, required: true },
-  image: { type: String, required: true }
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" }
 });
 
 module.exports = mongoose.model("Venue", venueSchema);
