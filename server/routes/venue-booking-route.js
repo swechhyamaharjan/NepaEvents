50feed50035ea75332bcad475e2ec667a8a2c208
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-03-25 19:14:10 +0545

    Add image upload functionality to venue booking route

--- server/routes/venue-booking-route.js
+++ server/routes/venue-booking-route.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const { bookVenue, getAllVenueBooking, approveVenueBooking, rejectVenueBooking, verifyPayment, makePaymentForVenue, sendReceipt } = require("../controller/venue-booking-controller");
 const verifyToken = require("../middleware/verify-token");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = "uploads/";
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.post('/', verifyToken, bookVenue)
+router.post('/', verifyToken,  upload.single("image"), bookVenue)
 router.get('/', verifyToken, getAllVenueBooking)
 router.put('/:id/approve', verifyToken, approveVenueBooking)
 router.put('/:id/reject', verifyToken, rejectVenueBooking)
