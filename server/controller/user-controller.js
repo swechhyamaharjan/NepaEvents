22e7452f10292735fff70415dd273d3903cf8c76
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-03-25 23:27:01 +0545

    Add getAllUsers function

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -101,8 +101,17 @@ const getUser = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error!" })
   }
 }
+// Fetch all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, '-password'); 
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch users', error });
+  }
+};
 
-module.exports = { registerUser, loginUser, logoutUser, getUser }
+module.exports = { registerUser, loginUser, logoutUser, getUser, getAllUsers }
 
 
 
