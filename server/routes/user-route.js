01142dd61982b17330045603d36828a2d6201022
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-04-05 13:21:13 +0545

    Added OTP and password reset routes to user API

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {registerUser, loginUser, logoutUser, getUser, getAllUsers} = require('../controller/user-controller');
+const {registerUser, loginUser, logoutUser, getUser, getAllUsers, sendOtp, verifyOtp, resetPassword} = require('../controller/user-controller');
 const verifyToken = require('../middleware/verify-token');
 
 const router = express.Router();
@@ -9,5 +9,8 @@ router.post('/login', loginUser)
 router.post('/logout', logoutUser)
 router.get('/getProfile', verifyToken, getUser)
 router.get('/list', getAllUsers);
+router.post("/sendOtp", sendOtp)
+router.post("/verifyOtp", verifyOtp)
+router.post("/resetPassword", resetPassword)
 
 module.exports = router;
