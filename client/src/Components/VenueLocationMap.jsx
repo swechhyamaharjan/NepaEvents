ba735173c530fa404ceac70dcb1d19facde2ba90
swechhyamaharjan <swekchya.mjn@gmail.com>
2025-04-09 21:54:03 +0545

    Fixed Leaflet marker icon paths and improve location marker logic

--- client/src/Components/VenueLocationMap.jsx
+++ client/src/Components/VenueLocationMap.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
-
-// Fix default marker icon issues
 import 'leaflet/dist/leaflet.css';
+
+// Fix Leaflet icon paths
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
@@ -11,6 +11,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
 });
 
+// Only allow setting new location if there's no existing one
 function LocationMarker({ setNewVenue }) {
   const [position, setPosition] = useState(null);
 
@@ -24,20 +25,31 @@ function LocationMarker({ setNewVenue }) {
     },
   });
 
-  return position === null ? null : (
-    <Marker position={position} />
-  );
+  return position ? <Marker position={position} /> : null;
 }
 
-export default function VenueLocationMap({ setNewVenue }) {
+export default function VenueLocationMap({ setNewVenue, existingCoordinates }) {
+  const parsedCoordinates = existingCoordinates
+    ? existingCoordinates.split(',').map(Number)
+    : null;
+
   return (
     <div className="h-64 w-full rounded-xl overflow-hidden border border-red-200">
-      <MapContainer center={[27.7172, 85.324]} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <MapContainer
+        center={parsedCoordinates || [27.7172, 85.324]}
+        zoom={13}
+        style={{ height: '100%', width: '100%' }}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <LocationMarker setNewVenue={setNewVenue} />
+
+        {/* Show existing marker if coordinates are available */}
+        {parsedCoordinates && <Marker position={parsedCoordinates} />}
+
+        {/* Only allow click interaction if no existing coordinates */}
+        {!parsedCoordinates && <LocationMarker setNewVenue={setNewVenue} />}
       </MapContainer>
     </div>
   );
